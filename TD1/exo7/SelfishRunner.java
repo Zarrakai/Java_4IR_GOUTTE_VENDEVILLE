fad763a2788cc5f4522d0c3a7a6daeac8e05f10d
Vendeville Leo <lvendevi@insa-20667>
2022-10-10 16:46:56 +0200

    fin exo 7

--- TD1/exo7/SelfishRunner.java
+++ TD1/exo7/SelfishRunner.java
@@ -1,20 +1,15 @@
 public class SelfishRunner extends Thread {
     private int tick = 1;
     private int num;
+    
     public SelfishRunner(int num) {
         this.num = num;
     }
     public void run() {
         while (tick < 400000) {
             tick++;
-            if ((tick % 50000) == 0){
+            if ((tick % 50000) == 0)
                 System.out.println("Thread #" + num + ", tick = " + tick);
-                try {
-                    sleep(10);
-                } catch (InterruptedException e) {
-                    // TODO: handle exception
-                }
-            }
         }
     }
-}
+    }
