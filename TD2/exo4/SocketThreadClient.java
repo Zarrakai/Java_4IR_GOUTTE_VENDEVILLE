68440f5f8d3f1c6ac2986ea3f3807f56907fe119
leoniegtt <leonie.goutte@gmail.com>
2022-10-14 14:21:55 +0200

    exo 4 ok

--- TD2/exo4/SocketThreadClient.java
+++ TD2/exo4/SocketThreadClient.java
@@ -3,9 +3,11 @@ import java.io.*;
 
 class SocketThreadClient {
 
+    /* attention au port choisi : réservés jusqu'à 1024 pour des protocoles */
     public static void main(String[] args) throws IOException{
         Socket link = null;
-        for (int i=0; i<1024; i++){
+        int depart = 1025, portsAAfficher = 250;
+        for (int i=depart; i<depart+portsAAfficher; i++){
             try {
                 link = new Socket("127.0.0.1", i);
                 System.out.println("server listening on port" + i);
--- TD2/exo4/SocketThreadServer.java
+++ TD2/exo4/SocketThreadServer.java
@@ -1,11 +1,24 @@
 import java.net.* ;
-import java.io.*;
+import java.io.* ;
 
 
 class SocketThreadServer {
 
     public static void main(String[] args) throws IOException{
-        ServerSocket servSocket = new ServerSocket(450);
+        
+        ServerSocket servSocket = new ServerSocket(1025);
+        Socket link = servSocket.accept();
+        System.out.println("connected");
+        link.close();
+        
+        ServerSocket servSocket2 = new ServerSocket(1234);
+        Socket link2 = servSocket2.accept();
+        System.out.println("connected");
+        link2.close();
+        
+        
+        /*
+        ServerSocket servSocket = new ServerSocket(1001);
         Socket link = servSocket.accept();
         System.out.println("connected");
         link.close();
@@ -14,7 +27,7 @@ class SocketThreadServer {
         Socket link2 = servSocket2.accept();
         System.out.println("connected");
         link2.close();
-
+*/
     }
 
 }
